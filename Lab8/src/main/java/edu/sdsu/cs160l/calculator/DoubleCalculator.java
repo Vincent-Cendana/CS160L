0cb9c7bff15392ade6e658485513e2c937207782
Vincent Cendana <vincent.cendana06@gmail.com>
2025-10-29 10:50:07 -0700

    Finished Lab 8

---
+++ Lab8/src/main/java/edu/sdsu/cs160l/calculator/DoubleCalculator.java
@@ -0,0 +1,23 @@
+package edu.sdsu.cs160l.calculator;
+
+public class DoubleCalculator implements Calculator<Double> {
+    @Override
+    public Double add(Double a, Double b) {
+        return a+b;
+    }
+
+    @Override
+    public Double sub(Double a, Double b) {
+        return a-b;
+    }
+
+    @Override
+    public Double div(Double a, Double b) throws ArithmeticException {
+        return a/b;
+    }
+
+    @Override
+    public Double mul(Double a, Double b) {
+        return a*b;
+    }
+}
--- Lab8/src/main/java/edu/sdsu/cs160l/calculator/IntegerCalculator.java
+++ Lab8/src/main/java/edu/sdsu/cs160l/calculator/IntegerCalculator.java
@@ -1,4 +1,23 @@
 package edu.sdsu.cs160l.calculator;
 
-public class IntegerCalculator {
+public class IntegerCalculator implements Calculator<Integer> {
+    @Override
+    public Integer add(Integer a, Integer b) {
+        return a+b;
+    }
+
+    @Override
+    public Integer sub(Integer a, Integer b) {
+        return a-b;
+    }
+
+    @Override
+    public Integer div(Integer a, Integer b) throws ArithmeticException {
+        return a/b;
+    }
+
+    @Override
+    public Integer mul(Integer a, Integer b) {
+        return a*b;
+    }
 }
